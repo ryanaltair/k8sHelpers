c93b6f2a6838388b0a2d0a68c63e26a6399e1554
ryanaltair_quicklane <al_tair@live.com>
2019-05-13 17:55:20 +0800

    fixed

--- app_pod.sh
+++ app_pod.sh
@@ -1,3 +1,3 @@
 
-apppod=$(kubectl get pods -n user-user-public -l type=server -o custom-columns=NAME:metadata.name | grep app)
+apppod=$(./../bin/kubectl --kubeconfig=./../suanpan.conf get pods -n user-user-public -l type=server -o custom-columns=NAME:metadata.name | grep app)
 ./../bin/kubectl --kubeconfig=./../suanpan.conf exec -it  ${apppod} -n user-user-public bash
